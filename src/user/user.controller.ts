b05b273ed2980c4396e44045bb580b02c7d35ad3
ArtemKurtiak <artem.k@magesoft.co>
2021-11-03 22:33:04 +0200

    Guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { RegisterUserDto } from "./dto/register-user.dto";
+import { CheckUserAgeGuard } from "./guards/check-user-age.guard";
 
 
 @Controller('/api/auth')
@@ -8,6 +9,7 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/register')
+  @UseGuards(CheckUserAgeGuard)
   registerUser(@Body() dto: RegisterUserDto) {
     return this.userService.registerUser(dto);
   }
